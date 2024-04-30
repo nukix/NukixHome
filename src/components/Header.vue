3be2639728ce0ccc7f6bfa72a53ea33d52547b08
nukix <aaa539687357@vip.qq.com>
2024-04-30 14:01:30 +0800

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>Header</div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
